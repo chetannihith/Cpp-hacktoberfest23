2af118e2d918899f638f0ed2196b9ef762f96fcc
Suyash Sachdeva <77083945+suyashsachdeva@users.noreply.github.com>
2023-10-23 11:37:57 +0530

    Create stack_from_scratch.cpp

---
+++ Bubble Sort/stack_from_scratch.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+using namespace std;
+
+class stack{
+    int stk[5];
+    int top;
+    public:
+        stack(){
+            top = -1;
+        }
+
+        void push(int x){
+            if (top>=4){
+                cout<<"stack overflow";
+                return;
+            }
+            stk[++top] = x;
+            cout<<"inserted "<<x;
+        }
+
+        void pop(){
+            if (top<0){
+                cout<<"Stack underflow";
+            }
+            cout<<"deleted "<<stk[top--];
+        }
+
+        void display(){
+            if(top<0){
+                cout<<" Stack empty "; return;}
+            for(int i =top; i>=0; i++)
+                cout<< stk[i]<<" ";
+
+        }
+};
+
+int main(){
+    int ch;
+    stack st;
+    while(1){
+        cout<<"\n1.push\n2.pop\n3.diaplay\n4.Exit\nPlease enter your choice";
+        cin>>ch;
+        switch(ch){
+            case 1:
+                cout << "Enter the element: ";cin >> ch;
+                st.push(ch);
+                break;
+            case 2:
+                st.pop();
+                break;
+            case 3:
+                st.display();
+                break;
+            case 4:
+            exit(0);
+        }
+    }
+
+    return 0;
+}

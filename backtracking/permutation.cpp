63b2b2a1d21335e00df6d29c32b2a4071cc90fc1
imgurujeet <146475585+imgurujeet@users.noreply.github.com>
2024-10-10 13:03:06 +0530

    code to some good problem of backtracking

---
+++ backtracking/permutation.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+// Github: https://github.com/imgurujeet
+
+void permutation(string &str, int i)
+{
+    if (i == str.size())
+    {
+        cout << str << "  ";
+        return;
+    }
+    for (int j = i; j < str.size(); j++)
+    {
+        swap(str[i], str[j]);
+        permutation(str, i + 1);
+        swap(str[i], str[j]);
+    }
+}
+
+int main()
+{
+    string str = "abc";
+    cout << "\n\n";
+    permutation(str, 0);
+    cout << "\n\n";
+    return 0;
+}
